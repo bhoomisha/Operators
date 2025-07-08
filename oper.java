ad9529f08f9d7fd6123ff66cb119569d78672a99
BHOOMI SHARMA <bhoomisharma3104@gmail.com>
2025-07-09 00:32:39 +0530

    operators is added

---
+++ oper.java
@@ -0,0 +1,64 @@
+public class operators{
+    public static void main(String args[]){
+        // 1 BINARY OPERATORS
+        /*int a=5;ls
+        int b=20;
+         System.out.println("add" +(a+b));
+         System.out.println("mul" +(a*b));
+        System.out.println("divide" +(b/a));
+        System.out.println("sub" +(a-b));
+        System.out.println("modulo" +(b%a));*/
+
+        // UNARY OPERATORS
+        /*1  int a=10;
+        int b=a++;
+        System.out.println(a);  //post increment  11
+         System.out.println(b);         //10    */
+
+        /* 2 int a=10;
+        int b= ++a;
+        System.out.println(a);               //11
+         System.out.println(b);      //11      */
+
+        /* 3 int a=10;
+        int b= a--;
+        System.out.println(a);     //9
+         System.out.println(b);    //10  */
+
+
+       /* 4  int a=10;
+        int b= --a;
+        System.out.println(a);     //9
+         System.out.println(b);    //9   */
+
+
+         // 2  RELATIONAL OPERATOR
+        /* int a=10;
+         int b=5;
+          System.out.println(a==b);
+           System.out.println(a>b);
+            System.out.println(a<b);
+             System.out.println(a>=b);
+              System.out.println(a<=b);
+               System.out.println(a!=b);  */
+
+        // 3 LOGICAL OPERATOR
+       /* System.out.println((3>2) &&(4==5));             //give ans in true and false
+        System.out.println((3>2) ||(4==5));
+        System.out.println(!(3>2)); */
+
+        //4 ASSIGNMENT OPERATOR
+       /* int A=10;
+        int B =5;
+           System.out.println( B +=5);
+             System.out.println(A-=4);
+             System.out.println(B*= 10);
+              System.out.println(A/=10);*/
+              
+             
+          
+
+
+
+    }
+}
